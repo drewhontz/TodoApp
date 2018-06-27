a219fec99d117431c77a6ae00b09c8f6b81fa3c9
Drew Hontz <athontz@gmail.com>
2018-06-27 16:38:15 -0700

    fixed port & db connections for mongo

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://heroku_48mwzsgb:rv17fa3vu9fospuu06ai8mlaq@ds115396.mlab.com:15396/heroku_48mwzsgb');
 
 module.exports = {mongoose};
