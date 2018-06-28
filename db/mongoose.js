6e283249bd7d25f05052f0e6cde98cbe55d61533
Drew Hontz <athontz@gmail.com>
2018-06-27 17:05:03 -0700

    fixed the mongo uri

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-console.log('mongo deets', process.env.MONGODB_URI);
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+console.log('mongo deets', process.env.MONGOLAB_PURPLE_URI);
+mongoose.connect(process.env.MONGOLAB_PURPLE_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
