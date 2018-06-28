a0df27e704bc8b0af7d2fc4e2cffe2e29ba94ad6
Drew Hontz <athontz@gmail.com>
2018-06-27 17:02:23 -0700

    testing mongo cnxn

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
+console.log('mongo deets', process.env.MONGODB_URI);
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
