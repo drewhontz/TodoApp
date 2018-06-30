17fafb17357b34760fa031f741947abd61f58927
Drew Hontz <athontz@gmail.com>
2018-06-30 08:33:23 -0700

    added JWT example from first principles

---
+++ playground/hashing.js
@@ -0,0 +1,22 @@
+const {SHA256} =  require('crypto-js');
+
+var message = 'I am user number 3';
+var hash = SHA256(message).toString();
+
+var data = {
+	id: 4
+}
+
+var token = {
+	data,
+	hash: SHA256(JSON.stringify(data) + 'some salt').toString()
+}
+
+token.data.id = 5;
+
+var resultHash = SHA256(JSON.stringify(token.data) + 'some salt').toString();
+if (resultHash === token.hash) {
+	console.log('Data was not changed');
+} else {
+	console.log('Data was changed. Do not trust!');
+}
