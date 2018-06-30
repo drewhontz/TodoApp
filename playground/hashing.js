4285365644431e34507c79475f39f1d91a54288e
Drew Hontz <athontz@gmail.com>
2018-06-30 08:44:18 -0700

    swapped first principles example for jwt module

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,22 +1,32 @@
-const {SHA256} =  require('crypto-js');
-
-var message = 'I am user number 3';
-var hash = SHA256(message).toString();
+const {SHA256} = require('crypto-js');
+const jwt = require('jsonwebtoken');
 
 var data = {
-	id: 4
-}
+	id: 10
+};
 
-var token = {
-	data,
-	hash: SHA256(JSON.stringify(data) + 'some salt').toString()
-}
+var token = jwt.sign(data, '123abc');
+var decoded = jwt.verify(token, '123abc');
 
-token.data.id = 5;
+console.log('decoded', decoded);
 
-var resultHash = SHA256(JSON.stringify(token.data) + 'some salt').toString();
-if (resultHash === token.hash) {
-	console.log('Data was not changed');
-} else {
-	console.log('Data was changed. Do not trust!');
-}
+// var message = 'I am user number 3';
+// var hash = SHA256(message).toString();
+
+// var data = {
+// 	id: 4
+// }
+
+// var token = {
+// 	data,
+// 	hash: SHA256(JSON.stringify(data) + 'some salt').toString()
+// }
+
+// token.data.id = 5;
+
+// var resultHash = SHA256(JSON.stringify(token.data) + 'some salt').toString();
+// if (resultHash === token.hash) {
+// 	console.log('Data was not changed');
+// } else {
+// 	console.log('Data was changed. Do not trust!');
+// }
